62d2951004b6e937002780d1e17ef0b2d5396e21
John Tucker <john@larkintuckerllc.com>
2018-05-29 06:04:36 -0400

    Roughly working

--- Assets/src/controllers/GameManager.cs
+++ Assets/src/controllers/GameManager.cs
@@ -1,10 +1,13 @@
 ﻿using Photon;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace com.larkintuckerllc.spheredemoar
 {
     public class GameManager : PunBehaviour
     {
+        public GameObject playerPrefab;
+
         public void LeaveRoom()
         {
             // TODO: REDUX STATE HERE
@@ -21,6 +24,12 @@ namespace com.larkintuckerllc.spheredemoar
         public override void OnLeftRoom()
         {
             SceneManager.LoadScene("Menu");
+        }
+
+        void Start()
+        {
+            if (PlayerManager.LocalPlayerInstance == null)
+                PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0f, 0f, 0f), Quaternion.identity, 0);
         }
     }
 }
---
+++ Assets/src/controllers/PlayerManager.cs
@@ -0,0 +1,16 @@
+﻿using Photon;
+using UnityEngine;
+
+public class PlayerManager : PunBehaviour {
+
+    public static GameObject LocalPlayerInstance;
+
+    void Awake()
+    {
+        if (photonView.isMine)
+        {
+            LocalPlayerInstance = gameObject;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
