9eab42baab944ebea299ec9412399dd4564cb693
John Tucker <john@larkintuckerllc.com>
2018-05-30 05:07:19 -0400

    Basic messaging

---
+++ Assets/src/controllers/Button.cs
@@ -0,0 +1,19 @@
+﻿using Photon;
+using ExitGames.Client.Photon;
+using UnityEngine;
+
+public class Button : PunBehaviour
+{
+    public void HandleClick()
+    {
+        var test = new Hashtable();
+        test.Add("a", "apple");
+        Debug.Log("CLICKED");
+        PhotonNetwork.room.SetCustomProperties(test);
+    }
+
+    public override void OnPhotonCustomRoomPropertiesChanged(Hashtable wow)
+    {
+        Debug.Log(wow);
+    }
+}
