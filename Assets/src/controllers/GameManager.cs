421c17e9e0a53c957704b4a6d713f813a555c5de
John Tucker <john@larkintuckerllc.com>
2018-05-30 05:12:55 -0400

    Remove Player Object

--- Assets/src/controllers/GameManager.cs
+++ Assets/src/controllers/GameManager.cs
@@ -1,16 +1,12 @@
 ﻿using Photon;
-using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace com.larkintuckerllc.spheredemoar
 {
     public class GameManager : PunBehaviour
     {
-        public GameObject playerPrefab;
-
         public void LeaveRoom()
         {
-            // TODO: REDUX STATE HERE
             PhotonNetwork.LeaveRoom();
         }
 
@@ -24,12 +20,6 @@ namespace com.larkintuckerllc.spheredemoar
         public override void OnLeftRoom()
         {
             SceneManager.LoadScene("Menu");
-        }
-
-        void Start()
-        {
-            if (PlayerManager.LocalPlayerInstance == null)
-                PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0f, 0f, 0f), Quaternion.identity, 0);
         }
     }
 }
